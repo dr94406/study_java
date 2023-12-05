3661e226e0229cf8c60b11255ef644ad73d6923c
김형민 <khm@edenbiz.co.kr>
2023-12-05 18:03:35 +0900

    [feat]: 실습 코드 추가

---
+++ src/study/OptionalPractice.java
@@ -0,0 +1,44 @@
+package study;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OptionalPractice {
+    public static void main(String[] args) {
+
+
+//        Optional<Member> optionalMember = Optional.of(new Member("khm",23,"RED"));
+//        optionalMember.ifPresent( member -> optionalMember.stream().forEach(System.out::println));
+//
+//        optionalMember.ifPresentOrElse(member -> optionalMember.stream().
+//                    forEach(System.out::println),
+//                    () -> Optional.empty());
+//
+//        System.out.println("optionalMember.isEmpty() = " + optionalMember.isEmpty());
+//
+//
+//
+//        Optional<Member> optionalEmptyMember = Optional.empty();
+//        if(optionalEmptyMember.isEmpty()){
+//            Optional.of(new Member("eden", 23, "RED")).stream().forEach(System.out::println);
+//        }
+//
+//        Optional<Member> optionalNotEmptyMember = Optional.of(new Member("khm",23,"BLUE"));
+//        if(optionalNotEmptyMember.isPresent()) {
+//            optionalNotEmptyMember.stream().forEach(System.out::println);
+//        }
+//
+//        /** Optional에는 값이 들어있을 수도 있고 null도 있을 수 있다. **/
+//        Optional<String> optionalEmptyCheck = Optional.ofNullable(null);
+//        optionalEmptyCheck.orElse(("good"));
+
+        List<String> good = Arrays.asList("khm","abc","dfd");
+
+        List<Member> entity = Arrays.asList(new Member("khm",23,"BLUE"));
+        System.out.println(entity.stream().filter(d -> good.contains(d.getName())).collect(Collectors.toList()));
+
+
+    }
+}
---
+++ src/study/StreamDoubleMapPractice.java
@@ -0,0 +1,45 @@
+package study;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDoubleMapPractice {
+
+    public static void main(String[] args) {
+
+
+
+        List<Member> memberList = Arrays.asList(new Member("khm", 23, "BLUE"));
+        memberList.stream();
+
+        List<String> test = Arrays.asList("khm","kdh","qwe");
+
+
+        Stream<String> testToStream = test.stream();
+
+//        testToStream.filter(testV -> testV.endsWith("3"))
+//                .map(String::toUpperCase)
+//                .forEach(System.out::println);
+
+//        testToStream.forEach( testCall ->{
+//            if(testCall.startsWith(testCall.toLowerCase())){
+//                System.out.println(testCall);
+//            }if(testCall.endsWith("M")){
+//                System.out.println(testCall);
+//            }
+//        });
+
+        /** 끝에 3이 포함되어 있으며 length가 5인 것 **/
+//        testToStream.filter(Testvalue -> Testvalue.contains("3")).filter(s -> s.length() == 5)
+//                .map(String::toUpperCase).forEach(System.out::println);
+//
+//        testToStream.forEach(System.out::println);
+//        test.forEach(System.out::println);
+
+//        testToStream.filter(n -> n.endsWith("M")).findFirst().stream().forEach(System.out::println);
+
+
+
+    }
+}
